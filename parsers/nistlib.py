9bef84ffa26bc31a3bda630e65a88c007bbc4a85
funnemonkee <ghostsmans@gmail.com>
2025-04-04 15:47:13 +0300

    Added method for retrieval of compound critical properties

--- parsers/nistlib.py
+++ parsers/nistlib.py
@@ -24,7 +24,11 @@ def get_data_for_ID(T, PLow, PHigh, PInc, ID):
     r = requests.get("https://webbook.nist.gov/cgi/fluid.cgi", params=payload)
     soup = BeautifulSoup(r.content, "html.parser")
     table = soup.find("table")
+    if len(table) == 0:
+        return None
     tr = table.find_all("tr")
+    if len(tr) == 0:
+        return None
     title_row = []
     result = {}
     for th in tr[0].find_all("th"):
@@ -39,5 +43,34 @@ def get_data_for_ID(T, PLow, PHigh, PInc, ID):
     return result
 
 
+def get_critical_values_for_ID(ID):
+    payload = {
+        "ID": "C"+str(ID),
+        "Units": "SI",
+        "Mask": "4\#Thermo-Phase",
+    }
+    r = requests.get("https://webbook.nist.gov/cgi/cbook.cgi", params=payload)
+    soup = BeautifulSoup(r.content, "html.parser")
+    table = soup.find("table")
+    tr_list = table.find_all("tr")
+    result = {}
+    name = soup.find("h1", {"id": "Top"})
+    if name:
+        result["name"] = name.text.strip()
+    for tr in tr_list:
+        td_list = tr.find_all("td")
+        if len(td_list) == 0:
+            continue
+        if td_list[0].text.strip() == "Pc" and "Pc" not in result.keys():
+            result["Pc"] = float(td_list[1].text.strip().split("±")[0])
+        if td_list[0].text.strip() == "Tc" and "Tc" not in result.keys():
+            result["Tc"] = float(td_list[1].text.strip().split("±")[0])
+
+    return result
+
+
 if __name__ == "__main__":
-    print(get_data_for_ID(T=140, PLow=1, PHigh=6, PInc=1, ID="C7732185"))
+    ids_to_fill = [7732185, 630080, 124389, 7727379, 7446095, 74828]
+    print(get_critical_values_for_ID(ids_to_fill[0]))
+    # for i in ids_to_fill:
+    #    print(f"results for {i}: {get_critical_values_for_ID(i)}")
